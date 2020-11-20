e633e8a226248fc5371d4634152f3f32632f84ea
Javier Meitín Moreno <jmeitin@ucm.es>
2020-11-20 17:27:42 +0100

    Player.js

--- game.js
+++ game.js
@@ -2,7 +2,9 @@ export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: "main" });
   }
-  preload() {}
+  preload() {
+    
+  }
 
   create() {
     this.add.text(10, 10, "¡Hola, mundo!", { fontColor: 0xffff00 });
