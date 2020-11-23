ac660432b64f89db6b236e0b1c4dae9be6acf276
Sebastian Sanchez-Hombria <sebsan01@ucm.es>
2020-11-23 16:32:23 +0100

    El personaje no quiere estar en pantalla
    Añadida clase sprites, que ayuda a crear cosas con fisica.
    Un monton de codigo comentado y lioso pq intente muchas cosas.

--- game.js
+++ game.js
@@ -1,14 +1,34 @@
+import Persona from "./Personajes/Persona.js"
+
 export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: "main" });
   }
-  preload() {
-    
+  preload() {   
+    this.load.image('guy', './Imgs/jugador1.png');
+    //this.load.spritesheet('guy', './Imgs/golem.png', 80, 111, {frameWidth:16, frameHeight:16});
+    //this.load.atlas('guyAtlas', './Imgs/golem.png');
   }
 
   create() {
     this.add.text(10, 10, "¡Hola, mundo!", { fontColor: 0xffff00 });
+    //this.add.image(500, 500, 'guy');
+
+    //this.player = new Persona(100,100,30, 'blue');
+    this.player = new Persona(this, 500, 500, "guy");
+    //this.player.draw();
+    
+    //this.input.onDown.
+    //this.add.sprite(200, 200, )
+    //golem = this.add.sprite(200, 200, 'guyAtlas');
+
   }
 
-  update(time, delta) {}
+  update(time, delta) {
+    this.player.moveLeft();
+    this.player.body.setVelocityX(0);
+    this.player.body.setVelocityY(0);
+  }
+
+
 }
--- index.js
+++ index.js
@@ -22,4 +22,4 @@ class Player {
 }
 
 const player = new Player(100,100,30, 'blue')
-player.draw()
+//player.draw()
