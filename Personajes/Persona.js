2e3c36771ce898afb13deebf1c0c95dbf4ff9c03
Sebastian Sanchez-Hombria <sebsan01@ucm.es>
2020-11-23 19:37:36 +0100

    limpiado todo lo anterior

--- Personajes/Persona.js
+++ Personajes/Persona.js
@@ -1,54 +1,29 @@
-// import Vector2D from '../Vector2D/vector2D.js'
-// import Sprites from "../sprites.js";
-
 import Sprites from "../sprites.js"
 
 export default class Person extends Sprites{
 
-    // constructor(x, y){
-    //     this.posicionX = x;    
-    //     this.posicionY = y;  
-    // }   
-
-    // update(){
-    //     ellipse(this.posicionX, this.posicionY, 100, 100);
-    //     this.posicionX++;
-    // }
-
-    // devuelvePos(){return this.posicionX + " " + this.posicionY};
-
-    // escribePos(){console.log(this.posicionX + " " + this.posicionY)};
-
     constructor(scene, x, y, type){
 
         super(scene, x, y, type);
-		this.x = x
-		this.y = y
+		this.x = x;
+		this.y = y;
 
 	}
-	
-	draw(){
-		c.beginPath()
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-		c.fillStyle = this.color
-		c.fill()
-    }
     
     moveLeft(){
         this.x--;
     }
 
     moveRight(){
-        this.x+= 20;
+        this.x++;
+    }
+
+    moveUp(){
+        this.y--;
+    }
+
+    moveDown(){
+        this.y++;
     }
 }
 
-
-
-// export default class Player extends Sprites{
-// 	constructor(scene, x, y, type){
-// 		super(scene, x, y, type);
-// 		this.setScale(0.5);
-// 		this.body.collideWorldBounds = true;
-// 	}
-// }
--- game.js
+++ game.js
@@ -6,28 +6,21 @@ export default class Game extends Phaser.Scene {
   }
   preload() {   
     this.load.image('guy', './Imgs/jugador1.png');
-    //this.load.spritesheet('guy', './Imgs/golem.png', 80, 111, {frameWidth:16, frameHeight:16});
-    //this.load.atlas('guyAtlas', './Imgs/golem.png');
+
   }
 
   create() {
     this.add.text(10, 10, "¡Hola, mundo!", { fontColor: 0xffff00 });
-    //this.add.image(500, 500, 'guy');
 
-    //this.player = new Persona(100,100,30, 'blue');
-    this.player = new Persona(this, 500, 500, "guy");
-    //this.player.draw();
+    this.player = new Persona(this, 300, 500, "guy");
     
-    //this.input.onDown.
-    //this.add.sprite(200, 200, )
-    //golem = this.add.sprite(200, 200, 'guyAtlas');
+    this.cursorKeys = this.input.keyboard.createCursorKeys();
 
   }
 
   update(time, delta) {
     this.player.moveLeft();
-    this.player.body.setVelocityX(0);
-    this.player.body.setVelocityY(0);
+
   }
 
 
