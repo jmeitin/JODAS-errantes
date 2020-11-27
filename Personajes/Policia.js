efeecdef2c7eb54f0d71d060ce76837057d4e975
Javier Meitín Moreno <jmeitin@ucm.es>
2020-11-27 17:50:27 +0100

    probando

--- Personajes/Policia.js
+++ Personajes/Policia.js
@@ -2,10 +2,33 @@ import Persona from "./Persona.js"
 
 export default class Policia extends Persona{
     constructor(scene, x, y, type, speed){
-        super(scene, x, y, type, speed);       
+        super(scene, x, y, type, speed);
+        
+        this.rangoVisual = 10;
     }
 
+    
+
     update(){
-        this.moveRight();
+        const dif = calcularDif();
+        
+        if (dif<= rangoVisual) {
+            this.moveLeft();
+        }
+
+        else {
+            this.moveRight();
+
+        }
+
+       
     }
+
+    calcularDif (){
+        return 0;
+    }
+
+
+
+
 }
