cc358fd0080a8275730a32079159c6fcad45be84
adriac02 <adriac02@ucm.es>
2020-12-01 03:07:12 +0100

    Array de civiles moviéndose

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+import Civil from "./Personajes/Civil.js";
 import Player from "./Personajes/Player.js";
 import Policia from "./Personajes/Policia.js";
 
@@ -7,6 +8,7 @@ export default class Game extends Phaser.Scene {
   }
   preload() {   
     this.load.image('guy', './Imgs/jugador1.png');
+    this.load.image('civil', './Imgs/viandante0.png');
     // this.load.spritesheet('golem', './Imgs/golem.png',
     // {
     //   frameHeight: 32,
@@ -21,12 +23,21 @@ export default class Game extends Phaser.Scene {
 
     this.player = new Player(this, 300, 600, "guy", this.cursorKeys, 10);
     this.policia = new Policia(this, 300, 600, "guy", 0.5);
+    this.civiles = [];
+    for(let i = 0; i < 10; i++){
+      let civil = new Civil(this, 600 + 70 * i, 600, "civil", 1).setScale(3);
+      this.civiles.push(civil);
+    }
 
   }
 
   update(time, delta) {
+    console.debug(this.civiles.length);
     this.player.movementManager();
     this.policia.update();
+    for(let i = 0; i < 10; i++){
+      this.civiles[i].update();
+    }
   }
 
 
