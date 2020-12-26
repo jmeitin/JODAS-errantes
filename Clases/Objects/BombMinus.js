2af9eaf2b9184f1c6f415e9389bfa8f55a0b0983
davidrodriguezz <davidr13@ucm.es>
2020-12-26 19:01:27 +0100

    POSIBLE DESTRUCCION

--- Clases/Objects/BombMinus.js
+++
@@ -1,7 +0,0 @@
-import object from './object.js';
-
-export default class bombminus extends object{
-    constructor(scene, x, y, type, peso){
-        super(scene, x, y, type, peso);
-    }
-}
--- Clases/Objects/Cape.js
+++
@@ -1,7 +0,0 @@
-import object from './object.js';
-
-export default class cape extends object{
-    constructor(scene, x, y, type, peso){
-        super(scene, x, y, type, peso);
-    }
-}
